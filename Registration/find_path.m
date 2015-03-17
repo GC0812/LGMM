ac32b36cab4ff14ec5207046480f263cb0b7724a
qiuxinxin <qxx1990421@163.com>
2015-03-17 21:45:34 +0800

    Update find_path.m

--- Registration/find_path.m
+++ Registration/find_path.m
@@ -1,14 +1,13 @@
 function [path] = find_path(linktable,loop_points)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+
 path_num=1;
 for j=1:numel(loop_points)
-        if(~isempty(find(linktable(:,loop_points(1))==linktable(1,loop_points(j))))) %看找到的数是否与第一个数相连，相连则作为路径，存放在path中
-            if(~isempty(find(linktable(:,loop_points(j))==linktable(1,loop_points(1)))))
-                path(path_num) = linktable(1,loop_points(j)); %有可能只找到3个以下的path吗？怎么确保path的第一个数是第一行的第一个数？
-                path_num=path_num+1;
-            end    
-        end   
+    if(~isempty(find(linktable(:,loop_points(1))==linktable(1,loop_points(j)), 1))) %看找到的数是否与第一个数相连，相连则作为路径，存放在path中
+        if(~isempty(find(linktable(:,loop_points(j))==linktable(1,loop_points(1)), 1)))
+            path(path_num) = linktable(1,loop_points(j)); 
+            path_num=path_num+1;
+        end
+    end
 end
 
 
