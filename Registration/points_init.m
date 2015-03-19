64fd8ed70614821398d12a2dadd25a3a955d6584
qiuxinxin <qxx1990421@163.com>
2015-03-19 18:10:57 +0800

    Update points_init.m

--- Registration/points_init.m
+++ Registration/points_init.m
@@ -1,7 +1,5 @@
 function [ptbifu] = points_init(bw)
 
-% This function initializes the feature points from the binary image
-
 [M, N]= size(bw);
 imdim = M*N + 1;
 
@@ -24,27 +22,25 @@ for k =1:npix
 end
 
 bw1 = (countmap>=3);
-[labelmap, numlabel] = bwlabel(bw1, 8);
-STATS1=regionprops(labelmap,'Centroid'); %求取每个连通区域的中心点的坐标
+[labelmap, numlabel] = bwlabel(bw1, 8); %得到numlabel个8连通区域
 NUM=regionprops(labelmap,'Area'); %求得每个连通区域的像素数目
-bifu1=zeros(numlabel,2);ptbifu1=zeros(numlabel,1);bifu_num=zeros(numlabel,1);
+
+bifu_num=zeros(numlabel,1);
 for i=1:numlabel
-    bifu1(i,:)=round(STATS1(i,1).Centroid); 
-    ptbifu1(i,1)=(bifu1(i,1)-1)*M+bifu1(i,2);
     bifu_num(i,1)=NUM(i,1).Area;
 end
-   sequence_no=find(bifu_num>=6); %寻找需要进一步分离的分叉点
+   sequence_no=find(bifu_num>=6); %寻找需要进一步分离的连通区域
       
    m=1;l=1;reserve_no=[];
    for k=1:numel(sequence_no)
-       sequence(k).number=find(labelmap==sequence_no(k));
+       sequence(k).number=find(labelmap==sequence_no(k));%得到满足条件的区域序号
        no=numel(sequence(k).number);
-       if ~isempty(find(countmap(sequence(k).number)>=5, 1))||(numel(find(countmap(sequence(k).number)==4))>=3 && NUM(sequence_no(k),1).Area==6)%去除多个点聚集在一起的情况,只在多个点中选择一个作为分叉点 
-          reserve_no(l)=k;
+       if ~isempty(find(countmap(sequence(k).number)>=5, 1))||(numel(find(countmap(sequence(k).number)==4))>=3 && NUM(sequence_no(k),1).Area==6)%多个点聚集在一起的情况,只选择一个作为分叉点；处理一种特殊情况 
+          reserve_no(l)=k; %不再划分连通区域，仅选择一个点作为分叉点
           l=l+1;
        else
            if mod(no,2)==1
-               part_sequence1(m).number=sequence(k).number(1:floor(no/2));%将该分叉点分离为两个分叉点
+               part_sequence1(m).number=sequence(k).number(1:floor(no/2));%将该连通区域分离为两个区域
                part_sequence2(m).number=sequence(k).number(floor(no/2)+2:end);
                m=m+1;
            else
@@ -54,8 +50,11 @@ end
            end
        end
    end
-   ptbifu1(setdiff(sequence_no,sequence_no(reserve_no)))=[];
    
+   s1=setdiff(sequence_no,sequence_no(reserve_no));
+   STATS1=regionprops(labelmap,'Centroid'); %求取各连通区域的中心点的坐标
+   STATS1(s1)=[];
+
    n=1;labelmap2=zeros(M,N); %对分离的分叉点标记
    for j=1:m-1
        labelmap2(part_sequence1(j).number)=n;
@@ -63,11 +62,15 @@ end
        n=n+2;
    end
    STATS2=regionprops(labelmap2,'Centroid'); %求取分离分叉点连通区域的中心点的坐标
-   bifu2=zeros(2*(m-1),2);ptbifu2=zeros(2*(m-1),1);
-   for i=1:2*(m-1)
-       bifu2(i,:)=round(STATS2(i,1).Centroid);
-       ptbifu2(i,1)=(bifu2(i,1)-1)*M+bifu2(i,2);
+   STATS=[STATS1;STATS2];
+   bifu=zeros(numel(STATS),2);ptbifu=zeros(numel(STATS),1);
+   for i=1:numel(STATS)
+       bifu(i,:)=round(STATS(i,1).Centroid);
+       ptbifu(i,1)=(bifu(i,1)-1)*M+bifu(i,2);
    end
    
-   ptbifu=[ptbifu1;ptbifu2];
 end
+
+
+
+
